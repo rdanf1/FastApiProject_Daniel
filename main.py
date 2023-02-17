43b3a609a4c0fec9b34e671110b05202e3181254
rdanf1 <reylet.daniel@gmail.com>
2023-02-17 23:30:44 +0100

    3d Commit : swagger tips see on route 127.0.0.1:8000/docs (API)  and some Caps...

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from fastapi import FastAPI
 
 from src.api import api
 
-app = FastAPI()
+app = FastAPI(title='MyAPI', version='0.0.0')
 
 app.include_router(api.routeur, prefix='/api')
--- src/api/api.py
+++ src/api/api.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter
 from src.api.endpoints import productWs
 
 routeur = APIRouter()
-routeur.include_router(productWs.routeur,prefix='/product',
-                       tags=["product"],
-                       responses={404: {"description": "impossible"}})
+routeur.include_router(productWs.routeur, prefix='/product',
+                       tags=["Product"],
+                       responses={404: {"description": "Impossible"}})
 
